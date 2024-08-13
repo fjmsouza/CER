ed0e1c71d8fd3a0620857a337b1cb37e53ba81a3
Flávio Souza <fjms@cin.ufpe.br>
2024-08-13 20:21:43 -0300

    some tests

--- model-lib-tmpl-main/model_xpto/main.py
+++ model-lib-tmpl-main/model_xpto/main.py
@@ -1,10 +1,13 @@
 """Implementation of the XPTO model."""
 
-import cython as cy
-import pandas as pd
+# import cython as cy  # noqa: ERA001
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
+model=LinearRegression(fit_intercept=True)
 
-def predict(data: pd.DataFrame) -> cy.float:
+# def predict(data: pd.DataFrame) -> cy.float:
+def predict(xfit):  # noqa: ANN001, ANN201, D417
     """Predict the output for the given data.
 
     Args:
@@ -17,10 +20,14 @@ def predict(data: pd.DataFrame) -> cy.float:
 
     """
     # TODO (@someone): Implement the prediction logic.
-    return 42
+    # return 42  # noqa: ERA001
+
+    return model.predict(xfit[:, np.newaxis])
 
 
-def train(data: pd.DataFrame) -> any:
+
+# def train(data: pd.DataFrame) -> any:
+def train (x,y):  # noqa: ANN001, ANN201, D417
     """Train the model using the given data.
 
     Args:
@@ -33,4 +40,19 @@ def train(data: pd.DataFrame) -> any:
 
     """
     # TODO (@someone): Implement the training logic.
-    return {'model': 'xpto'}
+
+    model.fit(x[:,np.newaxis], y)
+
+    # return {'model': 'xpto'}  # noqa: ERA001
+    return model
+
+
+
+np.random.seed(0)  # noqa: NPY002
+n=20   # Number of data points
+x=np.linspace(0, 10, n)
+y=x*2 + 1 + 1*np.random.Generator(n) # Standard deviation 1
+model = train(x,y)
+
+
+
--- model-lib-tmpl-main/tests/test_main_predict.py
+++ model-lib-tmpl-main/tests/test_main_predict.py
@@ -1,7 +1,15 @@
 """Arquivo de teste de predict."""
-from model_xpto.main import predict
+# from model_xpto.main import predict  # noqa: ERA001
+import random
+
+import model_xpto.main
+import numpy as np
 
 
-def test_predict_eq_42()->None:
+def test_predict__42()->None:
     """Este teste valida retorno = 42."""
-    assert predict(None)==42  # noqa: PLR2004
+    x = np.linspace(0,10,100)
+    index = random.randint(1,100)  # noqa: S311
+    y_hipotetic = x[index]*2 + 1
+
+    assert abs(model_xpto.main.predict(x) - y_hipotetic) < 1  # noqa: PLR2004, RUF100
