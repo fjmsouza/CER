560debd37a33428ef8ff11843d6339b21dd9c388
Flávio Souza <97055092+fjmsouza@users.noreply.github.com>
2024-10-26 14:20:28 -0300

    deleted useless folders

--- model-lib-tmpl-main/tests/test_main_predict.py
+++ model-lib-tmpl-main/tests/test_main_predict.py
@@ -6,10 +6,9 @@ from sklearn.metrics import accuracy_score
 import time
 import pytest
 
-@pytest.mark.repeat(5) 
+# @pytest.mark.repeat(5) 
 def test_predict_gt_70()->None:
     """Este teste valida retorno >=70%."""
     y_pred=model_xpto.main.predict(model_xpto.main.model, model_xpto.main.x_test)
     assert accuracy_score(model_xpto.main.y_test,y_pred) >= 0.7  # noqa: PLR2004, RUF100
-
     time.sleep(3)
